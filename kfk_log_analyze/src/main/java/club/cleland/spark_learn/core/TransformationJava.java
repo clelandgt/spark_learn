e2e5a4986a2a8c4df153b2b2ba53586061371afa
cleland <clelandgt@163.com>
2019-04-28 18:10:53 +0800

    new tranfromation file to store all transform

--- kfk_log_analyze/src/main/java/club/cleland/spark_learn/core/TransformationJava.java
+++ kfk_log_analyze/src/main/java/club/cleland/spark_learn/core/TransformationJava.java
@@ -8,17 +8,30 @@ import org.apache.spark.sql.SparkSession;
 
 import java.util.Arrays;
 
-public class MapJava {
-    public static void main(String[] args){
-        SparkSession spark = SparkSession
+
+public class TransformationJava {
+    private SparkSession spark;
+    private JavaSparkContext sc;
+    public TransformationJava() {
+        this.spark = SparkSession
                 .builder()
-                .appName("Map Test")
+                .appName("Transformtion Function")
                 .master("local[1]")
                 .getOrCreate();
-        JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
-        sc.setLogLevel("DEBUG");
+        this.sc = new JavaSparkContext(spark.sparkContext());
+        sc.setLogLevel("INFO");
+    }
 
-        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1,2,3,4,5));
+    public static void main(String[] args){
+        TransformationJava transformationJava = new TransformationJava();
+        transformationJava.map();
+    }
+
+    /**
+     * 对数据集合求平方：(1,2,3,4,5) -> (1,4,9,16,25)
+     */
+    public void map(){
+        JavaRDD<Integer> rdd = this.sc.parallelize(Arrays.asList(1,2,3,4,5));
         JavaRDD<Integer> resultRdd = rdd.map(new Function<Integer, Integer>() {
             public Integer call(Integer integer) throws Exception {
                 return integer * integer;
@@ -32,4 +45,6 @@ public class MapJava {
             }
         });
     }
+
+
 }
