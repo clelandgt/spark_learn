b1c73fc9b2874ec4489d8c8d1bd3603e625719a1
cleland <clelandgt@163.com>
2019-04-28 18:29:10 +0800

    update

--- kfk_log_analyze/src/main/java/club/cleland/spark_learn/core/TransformationJava.java
+++ kfk_log_analyze/src/main/java/club/cleland/spark_learn/core/TransformationJava.java
@@ -12,6 +12,19 @@ import java.util.Arrays;
 public class TransformationJava {
     private SparkSession spark;
     private JavaSparkContext sc;
+
+    public static JavaSparkContext getSc(){
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Transformtion Function")
+                .master("local[1]")
+                .getOrCreate();
+        JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
+        sc.setLogLevel("INFO");
+        return sc;
+
+    }
+
     public TransformationJava() {
         this.spark = SparkSession
                 .builder()
@@ -23,28 +36,48 @@ public class TransformationJava {
     }
 
     public static void main(String[] args){
-        TransformationJava transformationJava = new TransformationJava();
-        transformationJava.map();
+        map();
+        filter();
     }
 
     /**
      * 对数据集合求平方：(1,2,3,4,5) -> (1,4,9,16,25)
      */
-    public void map(){
-        JavaRDD<Integer> rdd = this.sc.parallelize(Arrays.asList(1,2,3,4,5));
-        JavaRDD<Integer> resultRdd = rdd.map(new Function<Integer, Integer>() {
+    public static void map(){
+        JavaSparkContext sc = getSc();
+        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1,2,3,4,5));
+        JavaRDD<Integer> resultRDD = rdd.map(new Function<Integer, Integer>() {
             public Integer call(Integer integer) throws Exception {
                 return integer * integer;
             }
+        });
+        prinfRDD(resultRDD);
+    }
 
+    /**
+     * 返回数据集中的偶数
+     */
+    public static void filter(){
+        JavaSparkContext sc = getSc();
+        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        JavaRDD<Integer> filterRDD = rdd.filter(new Function<Integer, Boolean>() {
+            public Boolean call(Integer integer) throws Exception {
+                return integer % 2 == 0;
+            }
         });
 
-        resultRdd.foreach(new VoidFunction<Integer>() {
-            public void call(Integer integer) throws Exception {
-                System.out.println(integer);
+        prinfRDD(filterRDD);
+    }
+
+    /**
+     * 打印RDD
+     *@param rdd
+     */
+    public static void prinfRDD(JavaRDD rdd){
+        rdd.foreach(new VoidFunction() {
+            public void call(Object o) throws Exception {
+                System.out.println(o);
             }
         });
     }
-
-
 }
